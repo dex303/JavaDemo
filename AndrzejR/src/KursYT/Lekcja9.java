159fa7d4b71441397214e694287eb19173e67803
Dex303 <mbbeurope@gmail.com>
2020-04-11 00:18:52 +0200

    kurs YT by AndrzejR Lekcja 5 - instrukcje warunkowe (if, switch) Lekcja 6 + Telewizor - wstęp do obiektowości Lekcja 9 - dziedziczenie Lekcja 10 - na podstawie lekcji 9 - polimorfizm

--- AndrzejR/src/KursYT/Lekcja9.java
+++ AndrzejR/src/KursYT/Lekcja9.java
@@ -26,5 +26,47 @@ public class Lekcja9 {
         sdi.nazwisko = "S.D.Inny";
         sdi.ObecnoscNaWykladzie();
         sdi.komunikat();
+
+        System.out.println("\nxXxXxXxXxXxXxXxXxXxXxXxXx\n");
+
+
+        // polimorfizm
+        // pozwala np. na zrobienie tablicy obiektów różnych typów, ale skorelowanych przez dziedziczenie
+
+        Student [] studentTable = new Student[4];
+
+        studentTable[0] = new StudentDzienny();
+        studentTable[1] = new StudentZaoczny();
+        studentTable[2] = new StudentDziennyInny();
+        studentTable[3] = new StudentOnLine();
+
+        for (int x = 0; x < studentTable.length; x++){
+            studentTable[x].ObecnoscNaWykladzie();
+        }
+
+        // dzięki polimorfizmowi możemy stworzyć obiekt gdzie
+        // zmienna jest typu Student ale obiekt jest typu StudentOnLine
+        Student s = new StudentOnLine();
+        wezStudenta(s);
+
+        System.out.println("\nxXxXxXxXxXxXxXxXxXxXxXxXx\n");
+        ObslugaStudenta st = new ObslugaStudenta();
+
+        StudentOnLine sto = new StudentOnLine();
+        StudentDzienny std = new StudentDzienny();
+
+        st.obsluz(sto);
+        st.obsluz(std);
+    }
+
+    public static void wezStudenta(Student s){
+        System.out.println("\nxXxXxXxXxXxXxXxXxXxXxXxXx\n");
+        s.ObecnoscNaWykladzie();
+    }
+}
+
+class ObslugaStudenta{
+    public void obsluz(Student st){
+        st.ObecnoscNaWykladzie();
     }
 }
